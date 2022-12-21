00736e448b0f368331360360afa7b37371100e3c
Jason Godesky <jefgodesky@gmail.com>
2022-12-21 16:38:58 -0500

    Set up home template
    Remove console.logs

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,28 @@
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
 import express, { Request, Response } from 'express'
 import dotenv from 'dotenv'
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT
 
+app.set('view engine', 'ejs')
+app.set('views', join(__dirname, '../src/views'))
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, Eberron!')
+  const title = 'The Dragon Between'
+  const description = 'Welcome to Eberron.'
+  const url = 'https://dragonbetween.net'
+  const image = '/images/social.jpg'
+  res.render('pages/home', {
+    title,
+    description,
+    twitter: { title, description, image },
+    og: { title, description, image, url }
+  })
 })
 
 app.listen(port, () => {

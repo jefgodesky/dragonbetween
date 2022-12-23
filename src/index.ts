b7dd76d5e46e3595ce958572c5f61695a1996e8f
Jason Godesky <jefgodesky@gmail.com>
2022-12-22 21:21:19 -0500

    Logging in with username and password

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { dirname, join } from 'path'
 import { fileURLToPath } from 'url'
 import express, { Request, Response } from 'express'
 import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
+import pb from './connect.js'
 import getDefaultHeadInfo from './types/head-info/get-default-head-info.js'
 
 const thisdir = dirname(fileURLToPath(import.meta.url))
@@ -9,6 +11,9 @@ dotenv.config()
 
 const app = express()
 const port = process.env.PORT ?? 3000
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
@@ -22,6 +27,12 @@ app.get('/', (req: Request, res: Response) => {
 app.get('/login', (req: Request, res: Response) => {
   const info = getDefaultHeadInfo()
   res.render('pages/login', info)
+})
+
+app.post('/login', async (req: Request, res: Response) => {
+  const { username, password } = req.body
+  await pb.collection('users').authWithPassword(username, password)
+  res.redirect('/')
 })
 
 app.listen(port, () => {

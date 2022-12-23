30ebdc7cf00f5ab2ae60252d8da3671aa5b7abee
Jason Godesky <jefgodesky@gmail.com>
2022-12-23 13:58:06 -0500

    getCharacters: Load knowledge
    Remove debugging log

--- src/middlewares/get-characters.ts
+++ src/middlewares/get-characters.ts
@@ -4,7 +4,6 @@ import getCharacters from '../types/character/get-characters.js'
 
 const getCharactersMiddleware = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   req.characters = await getCharacters()
-  console.log(req.characters)
   next()
 }
 
--- src/types/character/get-characters.ts
+++ src/types/character/get-characters.ts
@@ -1,13 +1,23 @@
+import { Record } from 'pocketbase'
 import Character from './index.js'
+import getKnowledge from '../knowledge/get-knowledge.js'
 import pb from '../../connect.js'
 
 export default async function getCharacters (): Promise<Character[]> {
   const { model, isValid } = pb.authStore
   const sort = 'name'
   const filter = `player = "${model?.id ?? ''}"`
+  const expand = 'knowledge(character).secret'
   const records = isValid && model?.username === undefined
-    ? await pb.collection('characters').getFullList(undefined, { sort })
-    : await pb.collection('characters').getFullList(undefined, { sort, filter })
-  console.log(records)
-  return []
+    ? await pb.collection('characters').getFullList(undefined, { sort, expand })
+    : await pb.collection('characters').getFullList(undefined, { filter, sort, expand })
+  const characters: Character[] = []
+  for (const record of records) {
+    const knowledgeRecords = record.expand['knowledge(character)']
+    const secrets = knowledgeRecords === undefined ? [] : knowledgeRecords.map((record: Record) => record.expand.secret).map((record: Record) => record.key)
+    const knowledge = await getKnowledge(...secrets)
+    const { name, strength, dexterity, constitution, intelligence, wisdom, charisma } = record
+    characters.push({ name, knowledge, strength, dexterity, constitution, intelligence, wisdom, charisma })
+  }
+  return characters
 }

8f2679402f170c47f44a7f8552f403ad0207ae54
Jason Godesky <jefgodesky@gmail.com>
2022-12-23 10:48:30 -0500

    Character type: Add Knowledge
    Characters in our stories: what do they know? Do they know things? Let's find out!

--- src/types/character/index.ts
+++ src/types/character/index.ts
@@ -1,5 +1,8 @@
+import Knowledge, { isKnowledge } from '../knowledge/index.js'
+
 interface Character {
   name: string
+  knowledge: Knowledge
   strength: number
   dexterity: number
   constitution: number
@@ -10,8 +13,9 @@ interface Character {
 
 const isCharacter = (obj: any): obj is Character => {
   if (obj === null || Array.isArray(obj) || typeof obj !== 'object') return false
-  const { name, strength, dexterity, constitution, intelligence, wisdom, charisma } = obj
+  const { name, knowledge, strength, dexterity, constitution, intelligence, wisdom, charisma } = obj
   if (name === undefined || typeof name !== 'string') return false
+  if (!isKnowledge(knowledge)) return false
   const scores = [strength, dexterity, constitution, intelligence, wisdom, charisma]
   return scores.reduce((acc: boolean, curr: boolean) => acc && curr !== undefined && typeof curr === 'number', true)
 }

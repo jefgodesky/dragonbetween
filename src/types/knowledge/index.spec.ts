150c24b1d8df4325986230d5227d451ed7952f8d
Jason Godesky <jefgodesky@gmail.com>
2022-12-22 22:18:09 -0500

    isKnowledge: Guard against functions
    Also, adopt consistent reject/accept language in test labels for type guards.

--- src/types/knowledge/index.spec.ts
+++ src/types/knowledge/index.spec.ts
@@ -2,67 +2,75 @@ import { expect } from 'chai'
 import { isKnowledge } from './index.js'
 
 describe('Knowledge', () => {
-  it('returns false if given null', () => {
+  it('rejects null', () => {
     expect(isKnowledge(null)).to.equal(false)
   })
 
-  it('returns false if given undefined', () => {
+  it('rejects undefined', () => {
     expect(isKnowledge(undefined)).to.equal(false)
   })
 
-  it('returns false if given false', () => {
+  it('rejects a function', () => {
+    expect(isKnowledge(() => {})).to.equal(false)
+  })
+
+  it('rejects false', () => {
     expect(isKnowledge(false)).to.equal(false)
   })
 
-  it('returns false if given true', () => {
+  it('rejects true', () => {
     expect(isKnowledge(true)).to.equal(false)
   })
 
-  it('returns false if given a number', () => {
+  it('rejects a number', () => {
     expect(isKnowledge(1)).to.equal(false)
   })
 
-  it('returns false if given a string', () => {
+  it('rejects a string', () => {
     expect(isKnowledge('true')).to.equal(false)
   })
 
-  it('returns false if given an array', () => {
+  it('rejects an array', () => {
     expect(isKnowledge([1, 2, 3])).to.equal(false)
   })
 
-  it('returns true if given an empty object', () => {
+  it('accepts an empty object', () => {
     expect(isKnowledge({})).to.equal(true)
   })
 
-  it('returns false if given an object with a null property', () => {
+  it('rejects an object with a null property', () => {
     expect(isKnowledge({ test: null })).to.equal(false)
   })
 
-  it('returns false if given an object with an undefined property', () => {
+  it('rejects an object with an undefined property', () => {
     expect(isKnowledge({ test: undefined })).to.equal(false)
   })
 
-  it('returns true if given an object with a false property', () => {
+  it('rejects an object with a function property', () => {
+    expect(isKnowledge({ test: () => {} })).to.equal(false)
+  })
+
+  it('accepts an object with a false property', () => {
     expect(isKnowledge({ test: false })).to.equal(true)
   })
 
-  it('returns true if given an object with a true property', () => {
+  it('accepts an object with a true property', () => {
     expect(isKnowledge({ test: true })).to.equal(true)
   })
 
-  it('returns false if given an object with a number property', () => {
+  it('rejects an object with a number property', () => {
     expect(isKnowledge({ test: 1 })).to.equal(false)
   })
 
-  it('returns false if given an object with a string property', () => {
+  it('rejects an object with a string property', () => {
     expect(isKnowledge({ test: 'true' })).to.equal(false)
   })
 
-  it('returns false if given an object with an array property', () => {
+  it('rejects an object with an array property', () => {
     expect(isKnowledge({ test: [1, 2, 3] })).to.equal(false)
   })
 
-  it('returns false if given an object with an object property', () => {
+  it('rejects an object with an object property', () => {
     expect(isKnowledge({ test: {} })).to.equal(false)
   })
 })

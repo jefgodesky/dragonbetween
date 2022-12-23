f6bbf973feb45ed5aab3ed593dcfe6606ce62b60
Jason Godesky <jefgodesky@gmail.com>
2022-12-23 09:45:29 -0500

    Add optional characters to Express Request type

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,8 +1,10 @@
-import HeadInfo from '../head-info'
+import Character from '../character.js'
+import HeadInfo from '../head-info.js'
 
 declare global {
   namespace Express {
     interface Request {
+      characters?: Character[]
       headInfo?: HeadInfo
     }
   }

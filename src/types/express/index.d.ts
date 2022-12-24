885221ee4c282518e625c1fa333d214bd112637c
Jason Godesky <jefgodesky@gmail.com>
2022-12-24 13:18:40 -0500

    Add optional knowledge to Express Request

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,9 +1,11 @@
 import Character from '../../interfaces/character/index.js'
 import HeadInfo from '../../interfaces/head-info/index.js'
+import Knowledge from '../../interfaces/knowledge/index.js'
 
 declare global {
   namespace Express {
     interface Request {
+      knowledge?: Knowledge
       viewInfo: {
         authenticated: boolean
         gm: boolean

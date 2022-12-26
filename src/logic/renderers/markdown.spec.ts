ee1d701507a0fa47ac5b75188a57d9dfd9c929ed
Jason Godesky <jefgodesky@gmail.com>
2022-12-26 14:47:29 -0500

    renderMarkdown: Hide secrets
    Any HTML tag with a data-secret attribute containing a secret that the POV character doesn't know is removed from the tree prior to rendering. Also formatting the HTML for better source to view.
    Simplify if statement

--- src/logic/renderers/markdown.spec.ts
+++ src/logic/renderers/markdown.spec.ts
@@ -4,12 +4,12 @@ import renderMarkdown from './markdown.js'
 describe('renderMarkdown', () => {
   it('renders Markdown into HTML', async () => {
     const html = await renderMarkdown('*emphasis* and **strong**')
-    expect(html).to.equal('<p><em>emphasis</em> and <strong>strong</strong></p>')
+    expect(html).to.equal('\n<p><em>emphasis</em> and <strong>strong</strong></p>\n')
   })
 
   it('allows basic HTML', async () => {
     const html = await renderMarkdown('<span class="test">This is a test.</span>')
-    expect(html).to.equal('<p><span class="test">This is a test.</span></p>')
+    expect(html).to.equal('\n<p><span class="test">This is a test.</span></p>\n')
   })
 
   it('sanitizes dangerous HTML', async () => {
@@ -19,41 +19,51 @@ describe('renderMarkdown', () => {
 
   it('allows divs', async () => {
     const html = await renderMarkdown('<div class="test">This is a test.</div>')
-    expect(html).to.equal('<div class="test">This is a test.</div>')
+    expect(html).to.equal('\n<div class="test">This is a test.</div>\n')
   })
 
   it('allows blockquotes', async () => {
     const html = await renderMarkdown('<blockquote>This is a test.</blockquote>')
-    expect(html).to.equal('<blockquote>This is a test.</blockquote>')
+    expect(html).to.equal('\n<blockquote>This is a test.</blockquote>\n')
   })
 
   it('allows figures', async () => {
     const html = await renderMarkdown('<figure><figcaption>This is a test.</figcaption></figure>')
-    expect(html).to.equal('<figure><figcaption>This is a test.</figcaption></figure>')
+    expect(html).to.equal('\n<figure>\n  <figcaption>This is a test.</figcaption>\n</figure>\n')
   })
 
   it('allows asides', async () => {
     const html = await renderMarkdown('<aside>This is a test.</aside>')
-    expect(html).to.equal('<aside>This is a test.</aside>')
+    expect(html).to.equal('\n<aside>This is a test.</aside>\n')
   })
 
   it('allows sections', async () => {
     const html = await renderMarkdown('<section>This is a test.</section>')
-    expect(html).to.equal('<section>This is a test.</section>')
+    expect(html).to.equal('\n<section>This is a test.</section>\n')
   })
 
   it('allows headers', async () => {
     const html = await renderMarkdown('<header>This is a test.</header>')
-    expect(html).to.equal('<header>This is a test.</header>')
+    expect(html).to.equal('\n<header>This is a test.</header>\n')
   })
 
   it('allows footers', async () => {
     const html = await renderMarkdown('<footer>This is a test.</footer>')
-    expect(html).to.equal('<footer>This is a test.</footer>')
+    expect(html).to.equal('\n<footer>This is a test.</footer>\n')
   })
 
   it('adds ID\'s to headings', async () => {
     const html = await renderMarkdown('## Test Heading\n\nHello, world!')
-    expect(html).to.equal('<h2 id="test-heading">Test Heading</h2>\n<p>Hello, world!</p>')
+    expect(html).to.equal('\n<h2 id="test-heading">Test Heading</h2>\n<p>Hello, world!</p>\n')
+  })
+
+  it('removes secrets you don\'t know', async () => {
+    const html = await renderMarkdown('<p>Not secret.</p><p data-secret="secret"><em>Shhh!</em></p>', { secret: false })
+    expect(html).to.equal('\n<p>Not secret.</p>\n')
+  })
+
+  it('shows secrets you know', async () => {
+    const html = await renderMarkdown('<p>Not secret.</p><p data-secret="secret"><em>Shhh!</em></p>', { secret: true })
+    expect(html).to.equal('\n<p>Not secret.</p>\n<p><em>Shhh!</em></p>\n')
   })
 })
--- src/logic/renderers/markdown.ts
+++ src/logic/renderers/markdown.ts
@@ -1,28 +1,43 @@
+import { Root, Element, RootContent } from 'hast'
+import Knowledge from '../../interfaces/knowledge/index.js'
 import deepmerge from 'deepmerge'
 import { unified } from 'unified'
 import remarkParse from 'remark-parse'
 import remarkGfm from 'remark-gfm'
 import remarkRehype from 'remark-rehype'
+import rehypeFormat from 'rehype-format'
 import rehypeRaw from 'rehype-raw'
+import rehypeRewrite from 'rehype-rewrite'
 import rehypeSanitize, { defaultSchema } from 'rehype-sanitize'
 import rehypeSlug from 'rehype-slug'
 import rehypeStringify from 'rehype-stringify'
+import clear from '../clear.js'
 
 const schema = deepmerge(defaultSchema, {
   tagNames: ['header', 'section', 'aside', 'footer'],
   attributes: { '*': ['className', 'style'] }
 })
 
-const processor = unified()
-  .use(remarkParse)
-  .use(remarkGfm)
-  .use(remarkRehype, { allowDangerousHtml: true })
-  .use(rehypeRaw)
-  .use(rehypeSanitize, schema)
-  .use(rehypeSlug)
-  .use(rehypeStringify)
-
-export default async function renderMarkdown (markdown: string): Promise<string> {
-  const markup = await processor.process(markdown)
-  return markup.toString()
+export default async function renderMarkdown (markdown: string, knowledge: Knowledge = {}): Promise<string> {
+  const render = await unified()
+    .use(remarkParse)
+    .use(remarkGfm)
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeRaw)
+    .use(rehypeRewrite, {
+      rewrite: (node: Root | RootContent, index: number | null, parent: Root | Element | null): void => {
+        if (parent !== null && node.type === 'element' && node.properties?.dataSecret !== undefined && !clear(knowledge, node.properties.dataSecret?.toString())) {
+          parent.children = parent.children.filter(child => {
+            if (child.position === undefined) return true
+            return JSON.stringify(child.position) !== JSON.stringify(node.position)
+          })
+        }
+      }
+    })
+    .use(rehypeSanitize, schema)
+    .use(rehypeSlug)
+    .use(rehypeFormat)
+    .use(rehypeStringify)
+    .process(markdown)
+  return render.toString()
 }

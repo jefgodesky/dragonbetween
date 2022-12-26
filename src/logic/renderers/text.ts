ea1519021ec533d32836d9a021928c7eddbadb60
Jason Godesky <jefgodesky@gmail.com>
2022-12-26 16:17:33 -0500

    Fix linting errors

--- src/logic/renderers/text.ts
+++ src/logic/renderers/text.ts
@@ -4,9 +4,9 @@ import populateWikiLinks from '../../interfaces/wiki-link/populate-wiki-links.js
 import renderWikiLinks from './wiki-links.js'
 import renderMarkdown from './markdown.js'
 
-export default async function renderText(orig: string, knowledge: Knowledge = {}): Promise<string> {
+export default async function renderText (orig: string, knowledge: Knowledge = {}): Promise<string> {
   const links = parseWikiLinks(orig)
   const populated = await populateWikiLinks(links, knowledge)
   const linked = renderWikiLinks(orig, populated)
-  return renderMarkdown(linked, knowledge)
+  return await renderMarkdown(linked, knowledge)
 }

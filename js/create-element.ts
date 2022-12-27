d938087e27daafa2938f101e37aff9501f196d26
Jason Godesky <jefgodesky@gmail.com>
2022-12-27 15:51:59 -0500

    Add new client-side method: createElement

---
+++ js/create-element.ts
@@ -0,0 +1,8 @@
+export default function createElement (tag: string, attributes: { [key: string]: string }, text?: string) {
+  const el = document.createElement(tag)
+  if (text !== undefined) el.appendChild(document.createTextNode(text))
+  for (const attr of Object.keys(attributes)) {
+    el.setAttribute(attr, attributes[attr])
+  }
+  return el
+}

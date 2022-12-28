727e627976415252968b6867dd25f103274626ab
Jason Godesky <jefgodesky@gmail.com>
2022-12-28 13:39:10 -0500

    Fix browse

--- src/middlewares/browse.ts
+++ src/middlewares/browse.ts
@@ -4,8 +4,9 @@ import pb from '../connect.js'
 import clear from '../logic/clear.js'
 
 const browse = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-  const roots = await pb.collection('categories').getFullList(undefined, { filter: 'parent = NULL' })
+  const roots = await pb.collection('categories').getFullList(undefined, { expand: 'category_categories(child)' })
   req.viewInfo.browse = roots
+    .filter(cat => JSON.stringify(cat.expand) === '{}')
     .filter(cat => clear(req.knowledge ?? {}, cat.secret))
     .map(cat => ({ url: `/categories/${cat.slug as string ?? ''}`, label: cat.name }))
   res.render('pages/browse', req.viewInfo)

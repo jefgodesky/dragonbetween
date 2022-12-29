971b25e70db615611a176bdb0454e26290710e18
Jason Godesky <jefgodesky@gmail.com>
2022-12-29 11:39:25 -0500

    loadSecretLegend: Load and organize secret messages

--- src/interfaces/secret-legend/load-secret-legend.spec.ts
+++ src/interfaces/secret-legend/load-secret-legend.spec.ts
@@ -1,13 +1,45 @@
 import { expect } from 'chai'
-import loadSecretLegend from './load-secret-legend.js'
+import SecretLegend from './index.js'
+import loadSecretLegend, { identifyCategories, organizeLegends } from './load-secret-legend.js'
+
+describe('identifyCategories', () => {
+  it('identifies categories', () => {
+    const actual = identifyCategories([
+      { label: 'Khorvairian', legend: {}, path: ['Languages', 'Mortal languages', 'Khorvairian'] },
+      { label: 'Riedran', legend: {}, path: ['Languages', 'Mortal languages', 'Riedran'] },
+      { label: 'Planar concepts', legend: {}, path: ['Planes', 'Planar concepts'] }
+    ]) as any
+    expect(JSON.stringify(actual.Languages['Mortal languages'].Khorvairian)).to.equal('{}')
+    expect(JSON.stringify(actual.Languages['Mortal languages'].Riedran)).to.equal('{}')
+    expect(JSON.stringify(actual.Planes['Planar concepts'])).to.equal('{}')
+  })
+})
+
+describe('organizeLegends', () => {
+  it('organizes secret legends', () => {
+    const categories = { Languages: { 'Mortal languages': { Khorvairian: {} } }, Planes: { 'Planar concepts': {} } }
+    const legends: SecretLegend[] = [
+      { legend: { 'KhorvairianFluency': 'Fluent in Khorvairian.' }, path: ['Languages', 'Mortal languages', 'Khorvairian'] },
+      { legend: { 'PlanarConceptRisia': 'Risia is about preservation.' }, path: ['Planes', 'Planar concepts'] }
+    ]
+    const actual = organizeLegends(legends, categories) as any
+    console.log(JSON.stringify(actual))
+    expect(actual[0].label).to.equal('Languages')
+    expect(actual[0].children[0].label).to.equal('Mortal languages')
+    expect(actual[0].children[0].children[0].label).to.equal('Khorvairian')
+    expect(actual[0].children[0].children[0].legend.KhorvairianFluency).to.equal('Fluent in Khorvairian.')
+    expect(actual[1].label).to.equal('Planes')
+    expect(actual[1].children[0].label).to.equal('Planar concepts')
+    expect(actual[1].children[0].legend.PlanarConceptRisia).to.equal('Risia is about preservation.')
+  })
+})
 
 describe('loadSecretLegend', () => {
-  it('loads the secret legend from the database', async () => {
-    const legend = await loadSecretLegend()
-    expect(legend.CommonKnowledgeKhorvaire).to.equal('You know all the things that anyone from Khorvaire would know.')
-    expect(legend.CommonKnowledgeBreland).to.equal('You know all the things that anyone from Breland would know.')
-    expect(legend.Dhakaan).to.equal('You know the name of the Empire of Dhakaan.')
-    expect(legend.SpellcastingBasics).to.equal('You understand the basics of spellcasting.')
-    expect(legend.ThisIsNotActuallyASecretKey).to.equal(undefined)
+  it('loads and organizes secret legends from the database', async () => {
+    const actual = await loadSecretLegend() as any
+    const labels = actual.map((cat: SecretLegend) => cat.label)
+    expect(labels).to.include('Languages')
+    expect(labels).to.include('Common knowledge')
+    expect(labels).to.include('Creatures')
   })
 })
--- src/interfaces/secret-legend/load-secret-legend.ts
+++ src/interfaces/secret-legend/load-secret-legend.ts
@@ -1,11 +1,44 @@
 import SecretLegend from './index.js'
+import Dictionary from '../dictionary/index.js'
+import NestedDictionary from '../nested-dictionary/index.js'
 import pb from '../../connect.js'
 
-export default async function getAllSecrets (): Promise<SecretLegend> {
-  const results = await pb.collection('secrets').getFullList(undefined, { sort: 'key' })
-  const legend: SecretLegend = {}
-  for (const result of results) {
-    legend[result.key] = result.legend
+const identifyCategories = (legends: SecretLegend[]): NestedDictionary => {
+  const categories: NestedDictionary = {}
+  for (const legend of legends) {
+    let index = categories
+    if (legend.path === undefined) continue
+    for (const item of legend.path) {
+      if (index[item] === undefined) index[item] = {}
+      index = index[item] as NestedDictionary
+    }
   }
-  return legend
+  return categories
 }
+
+const organizeLegends = (legends: SecretLegend[], category: NestedDictionary, path: string[] = []): SecretLegend[] => {
+  return Object.keys(category).map(key => {
+    let legend: Dictionary = {}
+    for (const record of legends) {
+      if ((record.path ?? []).join(' ') === [...path, key].join(' ')) legend = Object.assign(legend, record.legend)
+    }
+    return {
+      label: key,
+      legend,
+      children: organizeLegends(legends, category[key] as NestedDictionary, [...path, key])
+    }
+  })
+}
+
+export default async function loadSecretLegend (): Promise<SecretLegend[]> {
+  const results = await pb.collection('secrets').getFullList(undefined, { sort: 'key' })
+  const flat = results.map(result => {
+    const path = result.category.split('/').map((el: string) => el.trim())
+    const legend: { [key: string]: string } = {}
+    legend[result.key] = result.legend
+    return { path, legend }
+  })
+  return organizeLegends(flat, identifyCategories(flat))
+}
+
+export { identifyCategories, organizeLegends }

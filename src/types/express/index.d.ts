005bde0bfd5de4b8711669ee840d9b64f5e766db
Jason Godesky <jefgodesky@gmail.com>
2022-12-29 11:41:03 -0500

    Add optional character and secrets to Express Request

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -2,6 +2,7 @@ import Character from '../../interfaces/character/index.js'
 import HeadInfo from '../../interfaces/head-info/index.js'
 import Knowledge from '../../interfaces/knowledge/index.js'
 import Link from '../../interfaces/link/index.js'
+import SecretLegend from '../../interfaces/secret-legend/index.js'
 
 declare global {
   namespace Express {
@@ -12,7 +13,9 @@ declare global {
         gm: boolean
         pov: string
         returnUrl: string
+        character?: Character
         characters?: Character[]
+        secrets?: SecretLegend[]
         headInfo: HeadInfo
         browse?: Array<{ url: string, label: string }>
         lore?: {

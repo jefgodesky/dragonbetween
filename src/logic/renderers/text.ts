438496ebdba50fcb806e08a01768b5a6eb5e684a
Jason Godesky <jefgodesky@gmail.com>
2023-01-16 18:03:36 -0500

    Renderers: renderText: Add renderTimelines

--- src/logic/renderers/text.ts
+++ src/logic/renderers/text.ts
@@ -1,11 +1,13 @@
 import Knowledge from '../../interfaces/knowledge/index.js'
 import parseWikiLinks from '../parsers/wiki-links.js'
 import populateWikiLinks from '../../interfaces/wiki-link/populate-wiki-links.js'
+import renderTimelines from './timeline.js'
 import renderWikiLinks from './wiki-links.js'
 import renderMarkdown from './markdown.js'
 
 export default async function renderText (orig: string, knowledge: Knowledge = {}): Promise<string> {
-  const links = parseWikiLinks(orig)
+  const timelined = await renderTimelines(orig)
+  const links = parseWikiLinks(timelined)
   const populated = await populateWikiLinks(links, knowledge)
   const linked = renderWikiLinks(orig, populated)
   return await renderMarkdown(linked, knowledge)
